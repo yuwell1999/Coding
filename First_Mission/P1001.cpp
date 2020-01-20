7fa46109715bd07018d66047f09a9c529fbde0e8
yuwell1999 <50746474+yuwell1999@users.noreply.github.com>
2020-01-20 13:35:36 +0800

    Add files via upload

---
+++ First_Mission/P1001.cpp
@@ -0,0 +1,14 @@
+//
+// Created by YuYue on 2020/1/20.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b = 0;
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}

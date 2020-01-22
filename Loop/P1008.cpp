f1875d9505e95661d4362cfe4674f354ca184b29
yuwell1999 <50746474+yuwell1999@users.noreply.github.com>
2020-01-22 08:51:00 +0800

    Create P1008.cpp

---
+++ Loop/P1008.cpp
@@ -0,0 +1,11 @@
+//
+// Created by YuYue on 2020/1/20.
+//
+//将1,2, \cdots ,91,2,⋯,9共99个数分成33组，分别组成33个三位数，且使这33个三位数构成1:2:31:2:3的比例，试求出所有满足条件的33个三位数。
+
+#include <iostream>
+
+using namespace std;
+int main(){
+
+}
